7b741e4b4f96645ad7b2bd1afceec14dc3e0ac07
Isaac Andela <mail@isaacandela.nl>
2015-07-30 13:08:38 +0200

    Built patternlab-node from commit 28c2d25 on branch master

--- js/config.js
+++ js/config.js
@@ -1,5 +1,10 @@
+var baseURL = "/js/";
+if (location.pathname.split("/")[1] === "PatternLibrary") {
+  baseURL = "/PatternLibrary/js/";
+}
+
 System.config({
-  "baseURL": "/js/",
+  "baseURL": baseURL,
   "transpiler": "babel",
   "babelOptions": {
     "optional": [
--- js/directives/menuBar.js
+++
@@ -1,7 +0,0 @@
-import DopApp from 'DopApp';
-import controller from 'controllers/menuBarController';
-
-DopApp.directive("menuBar", () => ({
-	controllerAs: "vm",
-	controller
-}));
