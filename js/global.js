b245dbbaad2491259c0c07cd33ebff9a055fb0c7
Isaac Andela <mail@isaacandela.nl>
2015-08-05 10:51:00 +0200

    Built patternlab-node from commit 5deebd5 on branch master

--- js/global.js
+++ js/global.js
@@ -1,1 +1,8 @@
+System.config({
+  "baseURL": typeof location !== "undefined" &&
+      location.pathname.split("/")[1] === "PatternLibrary" ?
+          "/PatternLibrary/js/" :
+          "/js/"
+});
+
 System.import('start');
