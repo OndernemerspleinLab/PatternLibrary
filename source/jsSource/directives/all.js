28b908a245d018099f0bab422fc1e2a4c1989d60
Isaac Andela <mail@isaacandela.nl>
2015-11-11 23:04:33 +0100

    open close for onderwerpen tab on small screens and other visual tweaks

--- source/jsSource/directives/all.js
+++ source/jsSource/directives/all.js
@@ -1,5 +1,6 @@
 import 'directives/visualCloakDirective';
 import 'directives/menuBarDirective';
+import 'directives/openCloseSingleDirective';
 import 'directives/mainMenuDirective';
 import 'directives/countUpDirective';
 import 'directives/zoomTableDirective';
