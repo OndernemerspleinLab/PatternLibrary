1f91e15b759b316a947fbdec61115806cf8fcc18
Isaac Andela <mail@isaacandela.nl>
2015-12-03 12:37:03 +0100

    Built patternlab-node from commit 4beea5a on branch master

---
+++ js/directives/audienceSelector/audienceSelectorDirective.js
@@ -0,0 +1,16 @@
+import selectedAudienceStore from 'services/audienceSelector';
+import DopApp from 'DopApp';
+
+export const directiveName = "audienceSelector";
+
+class AudienceSelector {
+	constructor() {
+		this.selectedAudienceStore = selectedAudienceStore;
+	}
+}
+
+DopApp.directive(directiveName, () => ({
+	controllerAs: "viewModel",
+	controller: AudienceSelector,
+	bindToController: true,
+}));
---
+++ js/directives/personalisation/topTaskDirective.js
@@ -0,0 +1,21 @@
+import {topTasks, filterByAudience} from 'services/personalisationMockData';
+import {topTasks as templateUrl} from 'constants/templateUrls';
+import selectedAudienceStore from 'services/audienceSelector';
+
+import DopApp from 'DopApp';
+
+export const directiveName = "topTask";
+
+class TopTasks {
+	get topTasks() {
+		return filterByAudience(selectedAudienceStore.selectedAudience, topTasks);
+	}
+}
+
+DopApp.directive(directiveName, () => ({
+	templateUrl,
+	controllerAs: "viewModel",
+	controller: TopTasks,
+	scope: {},
+	bindToController: true,
+}));
